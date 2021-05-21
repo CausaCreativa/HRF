52f23bb5781707c5d3f2828c0925e1e1d255645c
Juan Dev <rexejej@gmail.com>
2021-05-21 18:54:54 -0500

    issues of HomeSection

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { ContainerContact } from "./DataContact"
-import { HomeSection,FormContact,Location} from "../../components"
+import { FormContact,Location} from "../../components"
 
 const Contact = () => {
 return (
     <>
     <ContainerContact>
-    <HomeSection></HomeSection>
+
     <FormContact></FormContact>
     <Location></Location>
     </ContainerContact>
--- src/pages/ServicePage/Service.js
+++ src/pages/ServicePage/Service.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { ContainerService } from "./DataService"
-import { HomeSection,ServicePlans} from "../../components"
+import {ServicePlans} from "../../components"
 const Service = () => {
 return (
     <>
     <ContainerService>
-    <HomeSection></HomeSection>
+    
     <ServicePlans></ServicePlans>
     </ContainerService>
     </>
