3ed1d398c902f406d323833d4467c03ec8300793
Juan Dev <rexejej@gmail.com>
2021-05-22 21:00:09 -0500

    hero component, already, upload with data

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { ContainerContact } from "./DataContact"
-import { FormContact,Location} from "../../components"
+import { ContainerContact, heroContact } from "./DataContact"
+import { FormContact,Location, Hero} from "../../components"
 
 const Contact = () => {
 return (
     <>
+    <Hero {...heroContact}/>
     <ContainerContact>
-
     <FormContact></FormContact>
     <Location></Location>
     </ContainerContact>
